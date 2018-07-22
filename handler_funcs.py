b3d9b30d53309aae87fc7eb71c350625dd58fbc3
dalekfodder <berk.buzcu@ozu.edu.tr>
2018-07-22 18:24:44 +0300

    Seperated db connection from the handler function

--- handler_funcs.py
+++ handler_funcs.py
@@ -1,22 +1,23 @@
 import sqlite3
+import dbconnection as db
+
 
 def start_handler_func(bot, update):
     update.message.reply_text(text='ok')
 
 
 def question_handler_func(bot, update, args):
-    conn = sqlite3.connect('cevap.db')
-    cur = conn.cursor()
+    conn = db.DbConnection('cevap.db')
 
     final_response = ""
 
     for x in args:
         try:
-            cur.execute('''SELECT answers.cevap FROM answers WHERE tags=? ''', [x])
+            conn.cursor.execute('''SELECT answers.cevap FROM answers WHERE tags=? ''', [x])
         except sqlite3.Error as err:
             print(err)
 
-        response1 = cur.fetchone()
+        response1 = conn.cursor.fetchone()
         if response1 is not None:
             final_response += response1[0]
             print(final_response)
@@ -26,23 +27,24 @@ def question_handler_func(bot, update, args):
     else:
         update.message.reply_text(text='öyle bişe yog')
 
-
-    cur.close()
-    conn.close()
     print(update.message.from_user)
 
+
 def add_question_handler(bot, update, args):
-    conn = sqlite3.connect('cevap.db')
-    cur = conn.cursor()
+    pass
+
 
 def authorize_handler(bot, update, args):
     pass
 
+
 def sticker_handler(bot, update):
     pass
 
+
 def on_join_handler(bot, update):
     pass
+
 
 def davet_at(bot, update):
     pass
