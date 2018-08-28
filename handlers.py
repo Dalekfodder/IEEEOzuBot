b7dbb1d05a6f8fcf188e408c30519f69b453c169
dalekfodder <berk.buzcu@ozu.edu.tr>
2018-08-29 02:44:41 +0300

    Follow up

--- handlers.py
+++ handlers.py
@@ -6,34 +6,31 @@ from telegram.ext import run_async, ConversationHandler
 
 from model import Answers, Admins
 
-CATEGORIES, TAGS = range(2)
+CATEGORIES, TAGS, FINAL = range(3)
 
 
-@run_async
-def question_handler(bot, update):
+def build_question_menu(bot):
     category_list = Answers.select(Answers.category_name).distinct()
     button_list = [[InlineKeyboardButton(category.category_name, callback_data="cat" + category.category_name)] for
                    category in category_list]
     button_list.append([InlineKeyboardButton("Bu listede yok.", callback_data="yok")])
-    reply_markup = InlineKeyboardMarkup(button_list)
+    menu = InlineKeyboardMarkup(button_list)
 
-    update.message.reply_text(text="Hangi konuda yardım almak istiyorsun?",
-                              reply_markup=reply_markup)
-    return CATEGORIES
+    return menu
 
 
 @run_async
-def callback_handler_back(bot, update):
-    callback = update.callback_query
+def main_question_handler(bot, update):
+    if update.callback_query is not None:
+        callback = update.callback_query
+        callback.answer()
+        callback.edit_message_text(text="Hangi konuda yardım almak istiyorsun?",
+                                   reply_markup=build_question_menu(bot))
 
-    category_list = Answers.select(Answers.category_name).distinct()
-    button_list = [[InlineKeyboardButton(category.category_name, callback_data="cat" + category.category_name)] for
-                   category in category_list]
-    button_list.append([InlineKeyboardButton("Bu listede yok.", callback_data="yok")])
-    reply_markup = InlineKeyboardMarkup(button_list)
-
-    callback.edit_message_text(text="Hangi konuda yardım almak istiyorsun?", reply_markup=reply_markup)
-    bot.answer_callback_query(callback_query_id=callback.id)
+    else:
+        update.message.reply_text(
+            text="Hangi konuda yardım almak istiyorsun?",
+            reply_markup=build_question_menu(bot))
 
     return CATEGORIES
 
@@ -45,10 +42,11 @@ def callback_handler_categories(bot, update):
 
     tag_list = Answers.select(Answers.tag_name).where(Answers.category_name == data).distinct()
     button_list = [[InlineKeyboardButton(tag.tag_name, callback_data="tag" + tag.tag_name)] for tag in tag_list]
-    button_list.append([InlineKeyboardButton("Geri git.", callback_data="geri")])
+    button_list.append([InlineKeyboardButton("Geri git.", callback_data="back")])
     reply_markup = InlineKeyboardMarkup(button_list)
 
     callback.edit_message_text(text="Hangi " + data + " hakkında bilgi almak istersin?", reply_markup=reply_markup)
+    callback.answer()
 
     return TAGS
 
@@ -62,17 +60,29 @@ def callback_handler_tags(bot, update):
     answers_list = Answers.select(Answers.answer).where(Answers.tag_name == data)
     for x in answers_list:
         reply_text += "\n" + x.answer
-    callback.edit_message_text(text=reply_text)
-    bot.answer_callback_query(callback_query_id=callback.id)
 
+    button_list = [[InlineKeyboardButton("Cevap yetersiz.", callback_data="bad")],
+                   [InlineKeyboardButton("Cevap yeterli.", callback_data="done")]]
+    reply_markup = InlineKeyboardMarkup(button_list)
+
+    callback.edit_message_text(text=reply_text, reply_markup=reply_markup)
+    callback.answer()
+
+    return FINAL
+
+
+@run_async
+def callback_handler_final(bot, update):
+    callback = update.callback_query
+    callback.answer()
+    callback.edit_message_text(callback.message.text)
     return ConversationHandler.END
 
 
 @run_async
 def callback_handler_custom_question(bot, update):
-    print("ok")
     callback = update.callback_query
-    bot.answer_callback_query(callback_query_id=callback.id)
+    callback.answer()
     return ConversationHandler.END
 
 
